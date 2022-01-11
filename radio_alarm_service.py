76eddc348fbaefd78e062782c99d28a97793dee4
Marc Hinterthaner <uni@hinterthaner.de>
2022-01-11 03:26:19 +0100

    plugin processing

--- radio_alarm_service.py
+++ radio_alarm_service.py
@@ -1,7 +1,7 @@
 import serial
 import time
 import os.path
-import datetime
+from datetime import datetime
 import requests
 from requests.structures import CaseInsensitiveDict
 import importlib
@@ -10,44 +10,67 @@ import configparser
 
 class RadioAlarmService:
     def __init__(self):
-        config = configparser.ConfigParser()
-        config.read('config/config.ini')
-        self.serial_port = config['RadioAlarmService']['serial_port']
-        self.serial_baudrate = config['RadioAlarmService']['serial_baudrate']
+        self.config = configparser.ConfigParser()
+        self.config.read('config/config.ini')
+        self.plugins = self.get_plugins()
+        self.serial = self.get_serial()
+        self.alarm = Alarm()
+
+    def get_plugins(self):
+        active_plugins = []
+        for key in list(self.config['Plugins'].keys()):
+            if self.config['Plugins'].getboolean(key):
+                active_plugins.append(key)
+        return active_plugins
+
+    def get_serial(self):
+        s = serial.Serial()
+        s.port = self.config['RadioAlarmService']['serial_port']
+        s.baudrate = self.config['RadioAlarmService']['serial_baudrate']
+        s.parity = serial.PARITY_NONE
+        s.stopbits = serial.STOPBITS_ONE
+        s.bytesize = serial.EIGHTBITS
+        s.timeout = None
+
+        if os.path.exists(self.serial.port):
+            print('Serial Port {0} exists.'.format(self.serial_port))
+            s.open()
+            print('Serial Port {0} is open.'.format(s.name))
+        return s
 
     def run(self):
-        while True:
-            if os.path.exists(self.serial_port):
-                print('Serial Port {0} exists.'.format(self.serial_port))
+        try:
+            while self.serial.isOpen():
+                # if serial data available
+                if self.serial.inWaiting():
+                    # read until stop byte
+                    message = self.serial.read_until(b'\x00').decode('iso_8859_1', 'ignore')  # bytes decoded to str
+                    self.log(message)
 
-                s = serial.Serial()
-                s.port = self.serial_port
-                s.baudrate = self.serial_baudrate
-                s.parity = serial.PARITY_NONE
-                s.stopbits = serial.STOPBITS_ONE
-                s.bytesize = serial.EIGHTBITS
-                s.timeout = None
-                s.open()
+                # if alarm exits and timeout expired
+                if self.alarm and (datetime.now() - self.alarm.last_update) > self.config['RadioAlarmService']['alarm_timeout']:
+                    self.process_plugins()
+                    self.alarm = None
 
-                print('Serial Port {0} is open.'.format(s.name))
-
-                try:
-                    while True:
-                        # print('try to read from serial')
-                        if s.inWaiting():
-                            # read until stop byte
-                            message = s.read_until(b'\x00').decode('iso_8859_1', 'ignore')  # .encode('utf-8')
-                            message = message.rstrip()  # remove newline on right end
-                            self.log(message)
-                        time.sleep(0.100)
-                except serial.SerialException as e:
-                    s.close()
-                except KeyboardInterrupt:
-                    s.close()
-            time.sleep(5)
+                time.sleep(0.100)
+        except serial.SerialException as e:
+            self.serial.close()
 
     def log(self, message):
-        f = open('message.log', 'a')
-        f.write('{0}: {1}\n'.format(datetime.datetime.now(), message))
+        f = open('logs/message.log', 'a')
+        f.write('{0}: {1}\n'.format(datetime.now(), message))
         f.close()
 
+    def process_plugins(self):
+        alarm_data = self.alarm.get_data()
+        for plugin in self._plugins:
+            plugin.process(self.config, alarm_data)
+
+
+class Alarm:
+    def __init__(self):
+        self.complete = False
+        self.last_update = None
+
+    def add_message(self, message):
+        self.last_update = datetime.now()
