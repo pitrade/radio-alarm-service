b96d77e97745b7355e35e36e5aceb10e76f3ab5e
Marc Hinterthaner <uni@hinterthaner.de>
2022-01-12 06:04:57 +0100

    implement plugin processing

--- radio_alarm_service.py
+++ radio_alarm_service.py
@@ -2,8 +2,6 @@ import serial
 import time
 import os.path
 from datetime import datetime
-import requests
-from requests.structures import CaseInsensitiveDict
 import importlib
 import configparser
 
@@ -12,31 +10,30 @@ class RadioAlarmService:
     def __init__(self):
         self.config = configparser.ConfigParser()
         self.config.read('config/config.ini')
-        self.plugins = self.get_plugins()
-        self.serial = self.get_serial()
-        self.alarm = Alarm()
+        self.plugins = []
+        self.serial = None
+        self.set_plugins()
+        self.set_serial()
+        self.alarm = Alarm()  # empty
 
-    def get_plugins(self):
-        active_plugins = []
+    def set_plugins(self):
         for key in list(self.config['Plugins'].keys()):
             if self.config['Plugins'].getboolean(key):
-                active_plugins.append(key)
-        return active_plugins
+                self.plugins.append(importlib.import_module('plugins.' + key, '.').Plugin())
 
-    def get_serial(self):
-        s = serial.Serial()
-        s.port = self.config['RadioAlarmService']['serial_port']
-        s.baudrate = self.config['RadioAlarmService']['serial_baudrate']
-        s.parity = serial.PARITY_NONE
-        s.stopbits = serial.STOPBITS_ONE
-        s.bytesize = serial.EIGHTBITS
-        s.timeout = None
+    def set_serial(self):
+        self.serial = serial.Serial()
+        self.serial.port = self.config['RadioAlarmService']['serial_port']
+        self.serial.baudrate = self.config['RadioAlarmService']['serial_baudrate']
+        self.serial.parity = serial.PARITY_NONE
+        self.serial.stopbits = serial.STOPBITS_ONE
+        self.serial.bytesize = serial.EIGHTBITS
+        self.serial.timeout = None
 
         if os.path.exists(self.serial.port):
-            print('Serial Port {0} exists.'.format(self.serial_port))
-            s.open()
-            print('Serial Port {0} is open.'.format(s.name))
-        return s
+            print('Serial Port {0} exists.'.format(self.serial.port))
+            self.serial.open()
+            print('Serial Port {0} is open.'.format(self.serial.name))
 
     def run(self):
         try:
@@ -46,31 +43,57 @@ class RadioAlarmService:
                     # read until stop byte
                     message = self.serial.read_until(b'\x00').decode('iso_8859_1', 'ignore')  # bytes decoded to str
                     self.log(message)
+                    self.alarm.add(message)
 
                 # if alarm exits and timeout expired
-                if self.alarm and (datetime.now() - self.alarm.last_update) > self.config['RadioAlarmService']['alarm_timeout']:
+                if self.alarm.last_update and (datetime.now() - self.alarm.last_update).total_seconds() > float(self.config['RadioAlarmService']['alarm_timeout']):
                     self.process_plugins()
-                    self.alarm = None
+                    self.alarm = Alarm()
 
                 time.sleep(0.100)
         except serial.SerialException as e:
             self.serial.close()
 
-    def log(self, message):
+    @staticmethod
+    def log(message):
         f = open('logs/message.log', 'a')
         f.write('{0}: {1}\n'.format(datetime.now(), message))
         f.close()
 
     def process_plugins(self):
-        alarm_data = self.alarm.get_data()
-        for plugin in self._plugins:
-            plugin.process(self.config, alarm_data)
+        for plugin in self.plugins:
+            plugin.process(plugin, self.config, self.alarm.data)
 
 
 class Alarm:
     def __init__(self):
-        self.complete = False
+        self.data = {}
         self.last_update = None
 
-    def add_message(self, message):
+    def add(self, message):
         self.last_update = datetime.now()
+        lines = message.splitlines()  # lines[0] is empty
+        t = lines[3].split(": #")
+        text = t[1].split(",", 11)  # text[6] = ?
+        address = text[4].strip().split(' ', 1)
+
+        if not self.data:
+            self.data['created_at'] = datetime.now()
+            self.data['datetime'] = datetime.strptime(lines[1], '%H:%M %d.%m.%y')
+            self.data['ric_list'] = []
+            self.data['ric_name_list'] = []
+            self.data['keyword'] = text[1].strip()
+            self.data['object_number'] = text[0].strip()
+            self.data['object'] = text[2].strip()
+            self.data['sub_object'] = text[3].strip()
+            self.data['street'] = address[0].strip()
+            self.data['house_number'] = address[1] if len(address) > 0 else None,
+            self.data['quarter'] = text[7].strip()
+            self.data['city'] = text[8].strip()
+            self.data['place'] = text[9].strip()
+            self.data['route'] = text[5].strip()
+            self.data['text'] = text[10].strip()
+            self.data['info'] = text[11].strip()
+
+        self.data['ric_list'].append(lines[2].strip())
+        self.data['ric_name_list'].append(t[0].strip())
