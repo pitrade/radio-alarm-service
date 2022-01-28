794f2f61a00e55c5053fb63f2b1fd17a2c114876
Marc Hinterthaner <uni@hinterthaner.de>
2022-01-28 10:15:19 +0100

    process news messages

--- plugins/divera.py
+++ plugins/divera.py
@@ -7,14 +7,15 @@ import json
 class Plugin:
     @staticmethod
     def process(self, config, alarm_data):
-        url = "https://www.divera247.com/api/alarm"
+        url_alarm = "https://www.divera247.com/api/alarm"
+        url_news = "https://www.divera247.com/api/news"
         api_key = config['Divera']['api_key']
 
         headers = CaseInsensitiveDict()
         headers["Content-Type"] = "application/json"
 
         data = {
-            "title": alarm_data['keyword'],
+            "title": alarm_data['keyword'] if 'keyword' in alarm_data else 'Info',
             "text": alarm_data['text'],
             "address": self.get_address(alarm_data),
             "ric": ','.join(alarm_data['ric_list']),
@@ -23,11 +24,14 @@ class Plugin:
 
         # print(json.dumps(data, ensure_ascii=False))
 
-        resp = requests.post(url, headers=headers, data=json.dumps(data))
+        resp = requests.post(url_alarm if 'keyword' in alarm_data else url_news, headers=headers, data=json.dumps(data))
         print(resp.status_code)
         print(resp.content)
 
     @staticmethod
     def get_address(alarm_data):
-        return '{0} {1} {2}'.format(alarm_data['street'], alarm_data['house_number'], alarm_data['quarter'])
+        if 'quarter' in alarm_data:
+            return '{0} {1} {2}'.format(alarm_data['street'], alarm_data['house_number'], alarm_data['quarter'])
+        else:
+            return ''
 
